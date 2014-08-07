eb99766562851fe84e7c4080f05e892775ef4774
Micheal Walls <michealpwalls@gmail.com>
2014-08-07 09:19:04 -0400

    Box model
    tables are SOOOO 1995 :)

--- Default.aspx
+++ Default.aspx
@@ -1,13 +1,18 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/public.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="thetaskmanager.Default" %>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder_header" runat="server">
     <div class="Header">
-        <h1>The Task Manager</h1>
+        <h1 id="title">The Task Manager</h1>
         <div id="LoginRegister">
             <a href="login.aspx">Login</a> / <a href="register.aspx">Register</a>
         </div>
     </div>
-    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder_body" runat="server">
     <div id="body">
         <p>Welcome to "The Task Manager", the single most amazing and majestic task manager you'll ever use! :)</p>
     </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder_footer" runat="server">
+    Copyright: Micheal Walls and Timothy Radder<br />
+        2014
 </asp:Content>
--- public.Master.designer.cs
+++ public.Master.designer.cs
@@ -13,21 +13,30 @@ namespace thetaskmanager {
     public partial class _public {
         
         /// <summary>
-        /// form1 control.
+        /// ContentPlaceHolder_header control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder_header;
         
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// ContentPlaceHolder_body control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder_body;
+        
+        /// <summary>
+        /// ContentPlaceHolder_footer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder_footer;
     }
 }
